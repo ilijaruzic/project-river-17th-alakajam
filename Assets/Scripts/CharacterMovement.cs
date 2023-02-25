9a18eebf6804d31902afb5202949042861e4c79b
Filip Popovic <fikisko161@gmail.com>
2023-02-25 01:18:13 +0100

    Added primitive movement.
    among other things kek

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 20f;
+    [SerializeField] private float movementSpeed = 50f;
+    [SerializeField] private float force = 5f;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * 0.5f * movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * force, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+}
