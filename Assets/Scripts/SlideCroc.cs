cf96343fe8a723ac6f78aa33885e4def2d742eee
Ilija Ružić <ilijaruzic@outlook.com>
2023-02-26 15:51:57 +0100

    Added basic croc slide script

--- Assets/Scripts/SlideCroc.cs
+++ Assets/Scripts/SlideCroc.cs
@@ -1,23 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SlideCroc : MonoBehaviour
 {
+    private Vector3 pos;
+    private Vector3 dis = new Vector3(0.5f, 0, 0);
+    [SerializeField] private float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos = transform.position;
         BoxCollider collider = GetComponent<BoxCollider>();
-        transform.Translate(Vector3.left * Time.deltaTime * 10f);
-        if (false) // ? transform.position.x ?? xx)
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * 10f);
-        }
+        transform.position = Vector3.Lerp(pos - dis, pos + dis, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
     }
 }
